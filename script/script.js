197c6506116a5debb1981d5d1a362c0089adb8f7
Gabriel Gomes <gabrielgomesc@outlook.com.br>
2022-10-21 14:07:56 -0300

    create habilidades section

---
+++ script/script.js
@@ -0,0 +1,22 @@
+function initNavegavcao() {
+  const tabMenu = document.querySelectorAll(`.js-tabmenu li`)
+  const activeClass = `ativo`
+  const tabContent = document.querySelectorAll(`.js-tabcontent li`)
+
+  if (tabMenu.length && tabMenu.length) {
+    tabContent[0].classList.add(activeClass)
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove(activeClass)
+      })
+      tabContent[index].classList.add(activeClass)
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener(`click`, () => {
+        activeTab(index)
+      })
+    })
+  }
+}
+initNavegavcao()
