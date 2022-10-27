c7e1a1d2c4c1e4210f186bb8b8698422f7bd3c54
Gabriel Gomes <gabrielgomesc@outlook.com.br>
2022-10-27 08:21:24 -0300

    corrections in files of images and ind script.js

--- script/script.js
+++ script/script.js
@@ -2,7 +2,6 @@ function initNavegavcao() {
   const tabMenu = document.querySelectorAll(`.js-tabmenu li`)
   const activeClass = `ativo`
   const tabContent = document.querySelectorAll(`.js-tabcontent li`)
-
   if (tabMenu.length && tabMenu.length) {
     tabContent[0].classList.add(activeClass)
     function activeTab(index) {
@@ -11,7 +10,6 @@ function initNavegavcao() {
       })
       tabContent[index].classList.add(activeClass)
     }
-
     tabMenu.forEach((itemMenu, index) => {
       itemMenu.addEventListener(`click`, () => {
         activeTab(index)
@@ -24,13 +22,11 @@ initNavegavcao()
 function initMenu() {
   const menuButton = document.querySelector(`[data-menu="button"]`)
   const menuLista = document.querySelector(`[data-menu="lista"]`)
-
   if (menuButton) {
     function openMenu(event) {
       menuLista.classList.toggle(`active`)
       menuButton.classList.toggle(`active`)
     }
-
     menuButton.addEventListener(`click`, openMenu)
   }
 }
@@ -38,22 +34,19 @@ initMenu()
 
 function initAnimacaoScroll() {
   const sections = document.querySelectorAll('.js-scroll');
-  if(sections.length) {
+  if (sections.length) {
     const windowMetade = window.innerHeight * 0.75;
-
     function animaScroll() {
       sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top;
         const isSectionVisible = (sectionTop - windowMetade) < 0;
-        if(isSectionVisible)
+        if (isSectionVisible)
           section.classList.add('ativo');
-        else 
+        else
           section.classList.remove('ativo');
       })
     }
-
     animaScroll();
-
     window.addEventListener('scroll', animaScroll);
   }
 }
