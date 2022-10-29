d3e08d4cf8a070fc988782e30a2101c9a5d9b77a
Gabriel Gomes <gabrielgomesc@outlook.com.br>
2022-10-29 19:29:20 -0300

    correctios in openMenu function

--- js/modules/openmenu.js
+++ js/modules/openmenu.js
@@ -3,17 +3,17 @@ import outsideClick from "./outsideclick.js"
 export default function initMenu() {
   const menuButton = document.querySelector(`[data-menu="button"]`)
   const menuLista = document.querySelector(`[data-menu="lista"]`)
-  const eventos = ['click', 'touchstart']
+  const clique = ['click']
   if (menuButton) {
-    function openMenu(event) {
+    function openMenu() {
       menuLista.classList.add('active')
       menuButton.classList.add('active')
-      outsideClick(menuLista, eventos, () => {
+      outsideClick(menuLista, clique, () => {
         menuLista.classList.remove(`active`)
         menuButton.classList.remove(`active`)
       })
     }
-    eventos.forEach((evento) => {
+    clique.forEach((evento) => {
       menuButton.addEventListener(evento, openMenu)
     })
   }
