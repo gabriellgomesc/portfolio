93eb8c4e5d02e256e8d146f95471ff2a08b2ea51
Gabriel Gomes <gabrielgomesc@outlook.com.br>
2022-10-29 07:38:44 -0300

    remove animation to scroll

--- script/script.js
+++ script/script.js
@@ -31,23 +31,3 @@ function initMenu() {
   }
 }
 initMenu()
-
-function initAnimacaoScroll() {
-  const sections = document.querySelectorAll('.js-scroll');
-  if (sections.length) {
-    const windowMetade = window.innerHeight * 0.75;
-    function animaScroll() {
-      sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = (sectionTop - windowMetade) < 0;
-        if (isSectionVisible)
-          section.classList.add('ativo');
-        else
-          section.classList.remove('ativo');
-      })
-    }
-    animaScroll();
-    window.addEventListener('scroll', animaScroll);
-  }
-}
-initAnimacaoScroll();
