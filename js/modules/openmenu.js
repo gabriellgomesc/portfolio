2d914b83d5a96eeb496a72a8bb3631cd80ed39db
Gabriel Gomes <gabrielgomesc@outlook.com.br>
2022-11-07 11:05:42 -0300

    JS standardization

--- js/modules/openmenu.js
+++ js/modules/openmenu.js
@@ -1,24 +1,23 @@
-import outsideClick from "./outsideclick.js"
+import outsideClick from './outsideclick.js';
 
 export default function initMenu() {
-  const menuButton = document.querySelector(`[data-menu="button"]`)
-  const menuLista = document.querySelector(`[data-menu="lista"]`)
-  const clique = ['click']
+  const menuButton = document.querySelector('[data-menu="button"]');
+  const menuLista = document.querySelector('[data-menu="lista"]');
+  const clique = ['click'];
+  function openMenu() {
+    menuLista.classList.remove('desactive');
+    menuButton.classList.remove('desactive');
+    menuLista.classList.add('active');
+    menuButton.classList.add('active');
+    outsideClick(menuLista, clique, () => {
+      menuLista.classList.remove('active');
+      menuButton.classList.remove('active');
+      menuLista.classList.add('desactive');
+      menuButton.classList.add('desactive');
+    });
+  }
+
   if (menuButton) {
-    function openMenu() {
-      menuLista.classList.remove('desactive')
-      menuButton.classList.remove('desactive')
-      menuLista.classList.add('active')
-      menuButton.classList.add('active')
-      outsideClick(menuLista, clique, () => {
-        menuLista.classList.remove(`active`)
-        menuButton.classList.remove(`active`)
-        menuLista.classList.add('desactive')
-      menuButton.classList.add('desactive')
-      })
-    }
-    clique.forEach((evento) => {
-      menuButton.addEventListener(evento, openMenu)
-    })
+    clique.forEach((evento) => { menuButton.addEventListener(evento, openMenu); });
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-import initNavegacao from './modules/navegacaotab.js'
-import initMenu from './modules/openmenu.js'
-import outsideClick from './modules/outsideclick.js'
+import initNavegacao from './modules/navegacaotab.js';
+import initMenu from './modules/openmenu.js';
+import outsideClick from './modules/outsideclick.js';
 
-initNavegacao()
-initMenu()
-outsideClick()
+initNavegacao();
+initMenu();
+outsideClick();
