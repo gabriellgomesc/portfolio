0898cf6f75b05d8c3c576cceccd638b729f7aadb
Gabriel Gomes <gabrielgomesc@outlook.com.br>
2022-12-12 14:36:09 -0300

    remove error of console

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 import initNavegacao from './modules/navegacaotab.js';
 import initMenu from './modules/openmenu.js';
-import outsideClick from './modules/outsideclick.js';
 
 initNavegacao();
 initMenu();
-outsideClick();
